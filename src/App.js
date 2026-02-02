b0ff77f3dbdcd538507335541ea47ee200849b51
Kazım Etiksan <kazim@pikselmutfak.com>
2026-02-02 19:02:43 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -13,11 +13,7 @@ import {
 import SignIn from './SignIn'
 import Home from './Home'
 
-import ReactGA from "react-ga4";
-
 const App = () => {
-
-  ReactGA.initialize("G-XY22F0HGZG");
 
   return (
     <>
