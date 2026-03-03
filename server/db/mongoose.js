29fdad4c7ea39b89654d7198de6e7657d3d02ced
kazimetiksan72 <kazim@etiksan.com>
2026-03-03 14:59:14 +0300

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://kazim_db_user:ffDt6HOiDSBJyNek@cluster0.vppuy6u.mongodb.net/?appName=Cluster0', {
+mongoose.connect('mongodb+srv://kazim_db_user:5NV1iUcqdr4wWpQe@cluster0.vppuy6u.mongodb.net/?appName=Cluster0', {
   serverSelectionTimeoutMS: 30000,
   socketTimeoutMS: 30000,
 })
